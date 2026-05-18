34b241f480747328194e5b14b169e3bd0da6f07d
Dev Éverllon <everllonsouza5@gmail.com>
2026-05-18 19:21:17 -0300

    autenticacacao

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\auth\LoginController;
 
 // Route::get('/welcome', function () {
 //     return view('welcome');
@@ -9,6 +10,16 @@ use App\Http\Controllers\SiteController;
 
 Route::get('/', [SiteController::class, 'index']);
 
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
+
+// Route::post('/login', function () {
+//     // Lógica de autenticação aqui
+//     return redirect('/');
+// });
 
 
 //MVC
